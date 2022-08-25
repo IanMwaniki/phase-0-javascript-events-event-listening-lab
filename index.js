ceabe3df4dde5b91b39c62dc9f65b0dc91e77259
Ian Maina Mwaniki <ian.mwaniki@student.moringaschool.com>
2022-08-25 15:39:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-function addingEventListener() {
-}
+    // function addEventListener(){
+    // const input = document.getElementById('input');
+    // input.addEventListener('click', function(){
+    // alert('I was clicked!')
+    // });
+    // };
+    // addEventListener();
+
+    // const input = document.getElementById('input');
+    // function clickAlert(){
+    //     alert('I was clicked!');
+    // }input.addEventListener('click', clickAlert);
+
+// const input = document.getElementById('input');
+// input.addEventListener('click', function() {
+//   alert('I was clicked!');
+// });
+
+let input;
+input = document.getElementById('input').addEventListener
+('click',function(){
+    return alert('Hi I was Clicked!')
+})
+
+
